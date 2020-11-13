d9d163372c70f3179e064e4be1d9b040f1f19778
김영한 <godppkyh@gmail.com>
2020-11-13 14:59:18 +0900

    Create StyleStream

---
+++ tsit/model/style_stream.py
@@ -0,0 +1,21 @@
+from tsit.model.misc import CSResBlk
+from tensorflow.keras import Model
+
+
+class StyleStream(Model):
+    def __init__(self, k):
+        super(StyleStream, self).__init__()
+        self.csresblk = []
+        for i in range(7):
+            self.csresblk.append(CSResBlk(k[i], k[i+1]))
+    def call(self, x):
+        content_feature = []
+        for i in range(7):
+            x = self.csresblk[i](x)
+            if i != 0:
+                content_feature.append(x)
+        return content_feature
+
+if __name__ == "__main__":
+    k = [64, 128, 256, 512, 1024, 1024, 1024, 1024]
+    cs = StyleStream(k)
