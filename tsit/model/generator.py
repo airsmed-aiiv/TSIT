62111aef75b8283007a1a62bddb669891016da25
김영한 <godppkyh@gmail.com>
2020-11-19 16:11:50 +0900

    Make generator.py and basic structure

---
+++ tsit/model/generator.py
@@ -0,0 +1,9 @@
+from tsit.model.misc import AdaIN, FADEResBlk
+from tensorflow.keras import Model
+from tensorflow.keras.layers import GaussianNoise
+
+
+class Generator(Model):
+    def __init__(self, k)
+
+
